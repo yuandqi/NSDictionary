409c7c13de0203fdd6e0d2764678531931db4ef3
legend <zhangchuanqi1@zhangchuanqideMac.local>
2015-06-30 16:30:21 +0800

    Initial Commit

---
+++ NSDictionary/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NSDictionary
+//
+//  Created by legend on 15/6/30.
+//  Copyright (c) 2015年 legend. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
