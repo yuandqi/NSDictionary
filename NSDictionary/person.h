677c785d4ea3f1a528a5aba41b903da098bc6d6e
legendzcq <499458212@qq.com>
2015-07-01 14:31:49 +0800

    yyyyy

---
+++ NSDictionary/person.h
@@ -0,0 +1,13 @@
+//
+//  person.h
+//  NSDictionary
+//
+//  Created by legend on 15/7/1.
+//  Copyright (c) 2015年 legend. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface person : NSObject
+
+@end
